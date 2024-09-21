aad82f0ce00cc77f7cfc179c8fbd6f9f6027d338
NR <nicholas.rebello@gmail.com>
2024-09-20 22:41:30 -0400

    Initial Commit

---
+++ Skystones/SkystonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkystonesApp.swift
+//  Skystones
+//
+//  Created by Nicholas Rebello on 2024-09-20.
+//
+
+import SwiftUI
+
+@main
+struct SkystonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
