d4d89a3260f92077b27e04c11d2036223c68354c
NR <nicholas.rebello@gmail.com>
2024-10-26 11:40:28 -0400

    set it to only display current player's pieces and altnerate per turn, and only show p1 in pvc.

--- Skystones/Views/PieceSelectionView.swift
+++ Skystones/Views/PieceSelectionView.swift
@@ -9,38 +9,57 @@ import SwiftUI
 
 struct PieceSelectionView: View {
     @ObservedObject var viewModel: GameViewModel
+    var showComputerPieces: Bool
 
     var body: some View {
         VStack() {
-            HStack {
-                // Player 1's pieces
-                ForEach(viewModel.player1Pieces.indices, id: \.self) { index in
-                    SkystoneView(skystone: viewModel.player1Pieces[index])
-                        .onTapGesture {
-                            viewModel.selectedPiece = viewModel.player1Pieces[index]
+            if (viewModel.currentPlayer == 1 || (showComputerPieces && viewModel.currentPlayer == 2)) {
+                if (viewModel.currentPlayer == 1) {
+                    HStack {
+                        // Player 1's pieces
+                        ForEach(viewModel.player1Pieces.indices, id: \.self) { index in
+                            SkystoneView(skystone: viewModel.player1Pieces[index])
+                                .onTapGesture {
+                                    viewModel.selectedPiece = viewModel.player1Pieces[index]
+                                }
+                                .border(viewModel.selectedPiece == viewModel.player1Pieces[index] ? Color.red : Color.clear)
                         }
-                        .border(viewModel.selectedPiece == viewModel.player1Pieces[index] ? Color.red : Color.clear)
-                }
-            }
-            .padding()
-
-            HStack {
-                // Player 2's pieces
-                ForEach(viewModel.player2Pieces.indices, id: \.self) { index in
-                    SkystoneView(skystone: viewModel.player2Pieces[index])
-                        .onTapGesture {
-                            viewModel.selectedPiece = viewModel.player2Pieces[index]
+                    }
+                    .padding()
+                    
+                } else {
+                    
+                    HStack {
+                        // Player 2's pieces
+                        ForEach(viewModel.player2Pieces.indices, id: \.self) { index in
+                            SkystoneView(skystone: viewModel.player2Pieces[index])
+                                .onTapGesture {
+                                    viewModel.selectedPiece = viewModel.player2Pieces[index]
+                                }
+                                .border(viewModel.selectedPiece == viewModel.player2Pieces[index] ? Color.red : Color.clear)
                         }
-                        .border(viewModel.selectedPiece == viewModel.player2Pieces[index] ? Color.red : Color.clear)
+                    }
+                    .padding()
                 }
+            } else {
+                HStack {
+                    // Player 1's pieces
+                    ForEach(viewModel.player1Pieces.indices, id: \.self) { index in
+                        SkystoneView(skystone: viewModel.player1Pieces[index])
+                            .onTapGesture {
+                                viewModel.selectedPiece = viewModel.player1Pieces[index]
+                            }
+                            .border(viewModel.selectedPiece == viewModel.player1Pieces[index] ? Color.red : Color.clear)
+                    }
+                }
+                .padding()
             }
-            .padding()
         }
     }
 }
 
 #Preview {
-    PieceSelectionView(viewModel: GameViewModel())
+    PieceSelectionView(viewModel: GameViewModel(), showComputerPieces: true)
 }
 
 
