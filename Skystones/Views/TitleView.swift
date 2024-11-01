cd8d2ad558e9d926f595fb822d12a49825aea2e6
NR <nicholas.rebello@gmail.com>
2024-11-01 01:04:41 -0400

    made titleview more mvvm as per what adam dahan said

--- Skystones/Views/TitleView.swift
+++ Skystones/Views/TitleView.swift
@@ -7,103 +7,148 @@
 
 import SwiftUI
 
+class TitleViewModel: ObservableObject {
+    @Published var showPVPView = false
+    @Published var showDifficultySelection = false
+    @Published var showInstructionsView = false
+}
+
 struct TitleView: View {
+    
+    private enum Constants {
+        static let gifName: String = "portal"
+        static let gifDiameter: CGFloat = 500
+        static let centeredGraphicImageName: String = "skystones-LC"
+        static let titleMusicString: String = "TitleMusic"
+        static let maxFrameDimension: CGFloat = 300
+        static let frameDimMultiplier: CGFloat = 0.6
+        static let pvpButtonTitle: String = "PVP (Player vs Player)"
+        static let pvcButtonTitle: String = "PVC (Player vs Computer)"
+        static let instructionButtonTitle: String = "Instructions"
+        static let cornerRadius: CGFloat = 10
+    }
+    
+    // MARK: - State Objects
+    
     @StateObject var audioManager = AudioPlayerManager()
-    @State private var showPVPView = false
-    //@State private var showPVCView = false
-    @State private var showDifficultySelection = false
-    @State private var showInstructionsView = false
+    @StateObject var viewModel = TitleViewModel()
+    
+    var background: some View {
+        Color.black
+    }
+    
+    var gifView: some View {
+        GIFView(gifName: Constants.gifName)
+            .scaledToFit()
+            .ignoresSafeArea()
+            .frame(width: Constants.gifDiameter, height: Constants.gifDiameter)
+    }
+    
+    var centeredGraphic: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                Image(Constants.centeredGraphicImageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: min(geometry.size.width * Constants.frameDimMultiplier, Constants.maxFrameDimension),
+                           height: min(geometry.size.width * Constants.frameDimMultiplier, Constants.maxFrameDimension))
+                    .ignoresSafeArea()
+                Spacer()
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
+    }
+    
+    var buttonToPVP: some View {
+        Button(action: {
+            audioManager.stopAudio()
+            viewModel.showPVPView = true
+        }) {
+            Text(Constants.pvpButtonTitle)
+                .font(.title2)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(Constants.cornerRadius)
+        }
+    }
+    
+    var buttonToPVC: some View {
+        Button(action: {
+            audioManager.stopAudio()
+            viewModel.showDifficultySelection = true
+        }) {
+            Text(Constants.pvcButtonTitle)
+                .font(.title2)
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(Constants.cornerRadius)
+        }
+    }
+    
+    var buttonToInstructions: some View {
+        Button(action: {
+            audioManager.stopAudio()
+            viewModel.showInstructionsView = true
+        }) {
+            Text(Constants.instructionButtonTitle)
+                .font(.title2)
+                .padding()
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(Constants.cornerRadius)
+        }
+    }
+    
+    var emptyTextView: some View {
+        Text("")
+    }
+    
+    var buttonVstack: some View {
+        VStack(spacing: 50) {
+            emptyTextView
+            emptyTextView
+            emptyTextView
+            emptyTextView
+            emptyTextView
+            emptyTextView
+
+            buttonToPVP
+            buttonToPVC
+            buttonToInstructions
+            
+        }
+        .padding()
+        .fullScreenCover(isPresented: $viewModel.showPVPView, onDismiss: {
+            audioManager.playAudio(named: Constants.titleMusicString) // Restart music when returning to TitleView
+        }) {
+            PVPView()
+        }
+        .fullScreenCover(isPresented: $viewModel.showDifficultySelection, onDismiss: {
+            audioManager.playAudio(named: Constants.titleMusicString) // Restart music when returning to TitleView
+        }) {
+            DifficultySelectionView(showPVCView: $viewModel.showDifficultySelection)
+        }
+        .fullScreenCover(isPresented: $viewModel.showInstructionsView, onDismiss: {
+            audioManager.playAudio(named: Constants.titleMusicString) // Restart music when returning to TitleView
+        }) {
+            InstructionsView()
+        }
+    }
 
     var body: some View {
         ZStack {
-            Color.black
             
-            GIFView(gifName: "portal")
-                .scaledToFit()
-                .ignoresSafeArea()
-                .frame(width: 500, height: 500)
+            background
+            gifView
+            centeredGraphic
             
-            GeometryReader { geometry in
-                VStack {
-                    Spacer()  // Push the image to the center vertically
-
-                    Image("skystones-LC")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: min(geometry.size.width * 0.6, 300),
-                               height: min(geometry.size.width * 0.6, 300))
-                        .ignoresSafeArea()
-
-                    Spacer()  // Push the image to the center vertically
-                }
-                .frame(width: geometry.size.width, height: geometry.size.height)  // Full screen frame
-            }
-            
-            VStack(spacing: 50) {
-                Text("")
-                Text("")
-                Text("")
-                Text("")
-                Text("")
-                Text("")
-                //Text("")
-
-                Button(action: {
-                    audioManager.stopAudio()
-                    showPVPView = true
-                }) {
-                    Text("PVP (Player vs Player)")
-                        .font(.title2)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-
-                Button(action: {
-                    audioManager.stopAudio()
-                    showDifficultySelection = true
-                }) {
-                    Text("PVC (Player vs Computer)")
-                        .font(.title2)
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                
-                Button(action: {
-                    audioManager.stopAudio()
-                    showInstructionsView = true
-                }) {
-                    Text("Instructions")
-                        .font(.title2)
-                        .padding()
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                
-            }
-            .padding()
-            .fullScreenCover(isPresented: $showPVPView, onDismiss: {
-                audioManager.playAudio(named: "TitleMusic") // Restart music when returning to TitleView
-            }) {
-                PVPView()
-            }
-            .fullScreenCover(isPresented: $showDifficultySelection, onDismiss: {
-                audioManager.playAudio(named: "TitleMusic") // Restart music when returning to TitleView
-            }) {
-                DifficultySelectionView(showPVCView: $showDifficultySelection)
-            }
-            .fullScreenCover(isPresented: $showInstructionsView, onDismiss: {
-                audioManager.playAudio(named: "TitleMusic") // Restart music when returning to TitleView
-            }) {
-                InstructionsView()
-            }
+            buttonVstack
         }
         .onAppear {
-            audioManager.playAudio(named: "TitleMusic")
+            audioManager.playAudio(named: Constants.titleMusicString)
         }
         .ignoresSafeArea()
     }
